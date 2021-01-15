ee3ca189c0e1d7ef94990f84fa9e1978ab03e990
apple <apple@appledeMacBook-Pro.local>
2021-01-15 10:10:00 +0800

    updated repo

--- src/components/Navbar/Navbar.svelte
+++ src/components/Navbar/Navbar.svelte
@@ -3,7 +3,11 @@
 </style>
 
 <script>
-    
+let isNav = false;
+function openNav () {
+    isNav === false ? isNav = true: isNav=false;
+    console.log(isNav)
+    }
 </script>
 <nav class="px-6 py-3 shadow">
     <div class="md:flex items-center justify-between">
@@ -16,16 +20,15 @@
     
             <!-- Mobile menu button -->
             <div class="md:hidden">
-                <button type="button" class="text-gray-500 dark:text-gray-200 hover:text-gray-600 dark:hover:text-gray-400 focus:outline-none focus:text-gray-600 dark:focus:text-gray-400" aria-label="Toggle menu">
-                    <svg viewBox="0 0 24 24" class="h-6 w-6 fill-current">
-                        <path d="M4 5h16a1 1 0 0 1 0 2H4a1 1 0 1 1 0-2zm0 6h16a1 1 0 0 1 0 2H4a1 1 0 0 1 0-2zm0 6h16a1 1 0 0 1 0 2H4a1 1 0 0 1 0-2z"></path>
-                    </svg>
+                <button type="button" class="text-gray-500 dark:text-gray-200 hover:text-gray-600 dark:hover:text-gray-400 focus:outline-none focus:text-gray-600 dark:focus:text-gray-400" aria-label="Toggle menu" on:click={openNav}>
+                    <img src={isNav?"../xtimes.svg":"../ham.svg"} alt="" class="border-0 focus:outline-none">
+
                 </button>
             </div>
         </div>
     
         <!-- Mobile Menu open: "block", Menu closed: "hidden" -->
-        <div class="flex flex-col -mx-2 mt-2 md:mt-0 md:flex-row md:block">
+        <div class="flex flex-col -mx-2 mt-2 md:mt-0 md:flex-row md:block {`${isNav? "block":"hidden"}`}" >
             <a href="#" class="py-2 px-2 text-sm text-gray-800 dark:text-gray-200 rounded hover:bg-gray-900 hover:text-gray-100 hover:font-medium md:mx-2">Home</a>
             <a href="#" class="py-2 px-2 text-sm text-gray-800 dark:text-gray-200 rounded hover:bg-gray-900 hover:text-gray-100 hover:font-medium md:mx-2">About Us</a>
             <a href="#" class="py-2 px-2 text-sm text-gray-800 dark:text-gray-200 rounded hover:bg-gray-900 hover:text-gray-100 hover:font-medium md:mx-2">Constitution </a>
