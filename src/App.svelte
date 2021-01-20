ae88bbaa7581aaf75a0733f5ef90329b0e94cb61
Isiaka Lukman Bamidele <isiakalukmandellyson@gmail.com>
2021-01-21 06:53:48 +0800

    retouched UI

--- src/App.svelte
+++ src/App.svelte
@@ -9,43 +9,48 @@ import Candidates from "./pages/Candidates.svelte";
 import Contact from "./pages/Contact.svelte";
 import Footer from "./components/Footer/Footer.svelte";
 import Login from "./pages/Login.svelte";
-import {Route} from "tinro"
+import {Route, router} from "tinro"
 import Manifesto from "./pages/Manifesto.svelte";
+import Transition from "./components/Transitions/Transition.svelte";
 
-let segment;
+router.mode.memory();
+router.subscribe(_=>window.scrollTo(0,0))
 
 </script>
 
-<NavBar {segment} />
+<NavBar />
 <main class=" overflow-x-hidden pt-9">
     <!-- <svelte:component this={page}/> -->
-    <Route path="/" >
-    <Index />
-    </Route>
-    <Route path="/about">
-    <About/>
-    </Route>
-    <Route path="/constitution">
-    <Constitution/>
-    </Route>
-    <Route path="/structure">
-    <Structure/>
-    </Route>
-    <Route path="/register">
-    <Register/>
-    </Route>
-    <Route path="/candidates">
-    <Candidates/>
-    </Route>
-    <Route path="/login">
-    <Login/>
-    </Route>
-    <Route path="/contact">
-    <Contact/>
-    </Route>
-    <Route path="/manifesto">
-    <Manifesto/>
-    </Route>
+    
+    <Transition>
+        <Route path="/" >
+            <Index />
+            </Route>
+            <Route path="/about">
+            <About/>
+            </Route>
+            <Route path="/constitution">
+            <Constitution/>
+            </Route>
+            <Route path="/structure">
+            <Structure/>
+            </Route>
+            <Route path="/register">
+            <Register/>
+            </Route>
+            <Route path="/candidates">
+            <Candidates/>
+            </Route>
+            <Route path="/login">
+            <Login/>
+            </Route>
+            <Route path="/contact">
+            <Contact/>
+            </Route>
+            <Route path="/manifesto">
+            <Manifesto/>
+            </Route>
+        </Transition>
 </main>
 
 <Footer />
